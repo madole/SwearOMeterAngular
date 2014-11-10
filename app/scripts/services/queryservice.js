9ee5f626e3b399a58a2cadc27063f8c0e4e2ceee
Andrew McDowell <madoliole@gmail.com>
2014-11-10 12:43:42 +1100

    more tidy up + comments

--- app/scripts/services/queryservice.js
+++ app/scripts/services/queryservice.js
@@ -9,41 +9,70 @@
  */
 angular.module('swearOmeterAngularApp')
   .service('queryService', function queryService($http) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    var queryResult = {},
-      cachedUsername = '';
+     var queryResult = {},
+       cachedUsername = '';
 
-    function queryTwitterApi(username) {
+     /**
+      * This function calls the Twitter Swear API hosted on heroku with the
+      * username the user has entered
+      * @param {String} username
+      * @returns {Object}
+      */
+     function queryTwitterApi(username) {
        cachedUsername = username;
 
-      return $http.get('http://afternoon-citadel-9782.herokuapp.com/getSwearWords?username='+ username +'')
-        .success(function(data) {
-          queryResult = data;
-          console.log(data);
-        }).error(function(err) {
-          console.log('FAILURE', err);
-        });
-    }
+       return $http.get('http://afternoon-citadel-9782.herokuapp.com/getSwearWords?username=' + username + '')
+         .success(cacheQueryResult).error(logError);
+     }
 
-    function getSwearTweets() {
-      return queryResult;
-    }
+     /**
+      * This function caches the query result data
+      * @param {Object} data
+      */
+     function cacheQueryResult(data) {
+       queryResult = data;
+       console.log(data);
+     }
 
-    function clearAll() {
-      queryResult = {};
-      cachedUsername = '';
-    }
+     /**
+      * This function logs the error message of the http request
+      * @param {Object|String} err
+      */
+     function logError(err) {
+       console.log('FAILURE', err);
+     }
 
-    function getUsername() {
-      return cachedUsername;
-    }
+     /**
+      * This function gets the cached swear tweets data
+      * @returns {Object}
+      */
+     function getSwearTweets() {
+       return queryResult;
+     }
 
-    return {
-      queryTwitterApi: queryTwitterApi,
-      getSwearTweets: getSwearTweets,
-      clearAll: clearAll,
-      getUsername: getUsername
-    };
+     /**
+      * This function clears the cached data
+      */
+     function clearAll() {
+       queryResult = {};
+       cachedUsername = '';
+     }
 
-  });
+     /**
+      * This function returns the cached username
+      * @returns {string}
+      */
+     function getUsername() {
+       return cachedUsername;
+     }
+
+     return {
+       queryTwitterApi: queryTwitterApi,
+       getSwearTweets: getSwearTweets,
+       clearAll: clearAll,
+       getUsername: getUsername
+     };
+
+   });
